507970032f32fd8d612c20860f0d21369922df7a
masud <masudranaacb@gmail.com>
2024-03-07 13:26:26 +0600

    hoisting is not working in es6

---
+++ style.js
@@ -0,0 +1,13 @@
+console.log(1)
+console.log(2)
+
+const displayFunction=()=>{
+    
+    console.log("hello")
+    addTowNumber()
+}
+console.log(4)
+console.log(5)
+
+const  addTowNumber=()=>console.log(8884)
+displayFunction()
